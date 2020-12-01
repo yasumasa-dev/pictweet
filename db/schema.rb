098722159f7db5af7533f8256a85cd617c787660
yasumasa-dev <72687417+yasumasa-dev@users.noreply.github.com>
2020-12-01 21:43:33 +0900

    カラムの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_144828) do
+ActiveRecord::Schema.define(version: 2020_12_01_124218) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.string "text"
     t.text "image"
     t.datetime "created_at", precision: 6, null: false
